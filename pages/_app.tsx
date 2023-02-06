8e9118288fe52587688413942fd0bd8c111d1fee
xapski <xapski2671@gmail.com>
2023-02-06 04:29:07 +0100

    cmt00

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import "@/styles/index.css"
+import type { AppProps } from "next/app"
+import { Navbar } from "@/containers/exportConts"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar/>
+      <Component {...pageProps} />
+    </>
+  )
 }
