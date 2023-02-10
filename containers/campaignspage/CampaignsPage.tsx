6af8dc6cc97b06df3b496ef3caabcfa792560d9f
xapski <xapski2671@gmail.com>
2023-02-10 09:36:10 +0100

    cmt13

---
+++ containers/campaignspage/CampaignsPage.tsx
@@ -0,0 +1,6 @@
+export default function CampaignsPage() {
+  
+  return (
+    <div>CampaignsPage</div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import Image from "next/image"
 import { Blog, Campaigns, Header, HowItWorks } from "@/containers/exportConts"
 
 
