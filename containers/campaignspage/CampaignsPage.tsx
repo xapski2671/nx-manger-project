a827f509a99d89f0b05a044189e8cb36a52b8281
xapski <xapski2671@gmail.com>
2023-02-13 15:12:56 +0000

    Feb 13, 2023, 4:12 PM

--- containers/campaignspage/CampaignsPage.tsx
+++ containers/campaignspage/CampaignsPage.tsx
@@ -5,7 +5,9 @@ export default function CampaignsPage() {
   return (
     <section className="ccp-section sc-padding fl-cl fl-c">
       <CategoryFilter/>
+      <h4>{"Featured"}</h4>
       <ActiveCampaign/>
+      <h4>{"Explore 320 Projects"}</h4>
       <CampaignGrid/>
     </section>
   )
