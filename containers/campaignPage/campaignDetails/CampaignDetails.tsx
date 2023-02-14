1d2f56cf818f4255e03d4c2adb472eaa201b0ea0
xapski <xapski2671@gmail.com>
2023-02-14 15:46:20 +0000

    Feb 14, 2023, 4:46 PM

--- containers/campaignPage/campaignDetails/CampaignDetails.tsx
+++ containers/campaignPage/campaignDetails/CampaignDetails.tsx
@@ -1,9 +1,22 @@
 export default function CampaignDetails() {
   return (
     <section className="cpd-section fl-tc">
-      <div className="cpd-trajectory">
+      <main className="cpd-trajectory">
+        <div className="cpd-cell-titles fl-cc">
+          <h4 className="cpd-cell-title">{"STORY"}</h4>
+          <h4 className="cpd-cell-title">{"RISKS"}</h4>
+          <h4 className="cpd-cell-title">{"REWARDS"}</h4>
+          <h4 className="cpd-cell-title">{"PAYMENT"}</h4>
+        </div>
+        <div className="cpd-separator"><div className="cpd-active-cell-indic"></div></div>
 
-      </div>
+        <div className="cpd-cell-story">
+          
+        </div>
+      </main>
+      <aside className="cpd-bio">
+
+      </aside>
     </section>
   )
 }
