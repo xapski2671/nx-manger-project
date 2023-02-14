d7a3d0330522bdb851cd3cfdeebf3586fca7270e
xapski <xapski2671@gmail.com>
2023-02-14 17:56:39 +0000

    Feb 14, 2023, 6:56 PM

--- components/rewardCard/RewardCard.tsx
+++ components/rewardCard/RewardCard.tsx
@@ -1,5 +1,14 @@
 export default function RewardCard() {
   return (
-    <div>RewardCard</div>
+    <div className="rc-container">
+      <div className="rc-no-reward">{"Pledge without a reward"}</div>
+      <div className="rc-input-container">
+        <div>
+          <p>{"Pledge amount"}</p>
+          <input type="number" />
+        </div>
+        <button className="rc-cta">{"Continue"}</button>
+      </div>
+    </div>
   )
 }
