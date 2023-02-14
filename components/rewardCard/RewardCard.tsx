7ff1f0b975bc0b0621aac7948c01f034a04270ae
xapski <xapski2671@gmail.com>
2023-02-14 18:11:10 +0000

    Feb 14, 2023, 7:11 PM

--- components/rewardCard/RewardCard.tsx
+++ components/rewardCard/RewardCard.tsx
@@ -1,7 +1,13 @@
+
 export default function RewardCard() {
   return (
     <div className="rc-container">
-      <div className="rc-no-reward">{"Pledge without a reward"}</div>
+      <div className="rc-no-reward">{"Pledge 0.5 ETH"}</div>
+
+      <div className="rc-description">
+        <p>{`${fetch("../../public/assets/reward-text.txt").then(r=>r.text()).then(text=>console.log(text))}`}</p>
+      </div>
+
       <div className="rc-input-container">
         <div>
           <p>{"Pledge amount"}</p>
