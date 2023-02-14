db9e6771f9c86e13e2c36de6423bd4dc446562c9
xapski <xapski2671@gmail.com>
2023-02-14 15:08:38 +0000

    Feb 14, 2023, 4:08 PM

--- containers/campaignPage/activeCampaign/ActiveCampaign.tsx
+++ containers/campaignPage/activeCampaign/ActiveCampaign.tsx
@@ -53,7 +53,7 @@ export default function ActiveCampaign() {
               {"Metaverse"}
             </div>
             <div className="acp-creator fl-cl">
-              <img src="/assets/manger_logo.svg" alt="bio-logo" />
+              <img src="/assets/manger_bio_logo.svg" alt="bio-logo" />
               <p>{"bullishmei"}</p>
             </div>
           </div>
