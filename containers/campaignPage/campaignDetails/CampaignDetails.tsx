f717353e7881487ec39b854bb946699f82de95ea
xapski <xapski2671@gmail.com>
2023-02-14 15:56:09 +0000

    Feb 14, 2023, 4:56 PM

--- containers/campaignPage/campaignDetails/CampaignDetails.tsx
+++ containers/campaignPage/campaignDetails/CampaignDetails.tsx
@@ -2,15 +2,15 @@ export default function CampaignDetails() {
   return (
     <section className="cpd-section fl-tc">
       <main className="cpd-trajectory">
-        <div className="cpd-cell-titles fl-cc">
-          <h4 className="cpd-cell-title">{"STORY"}</h4>
-          <h4 className="cpd-cell-title">{"RISKS"}</h4>
-          <h4 className="cpd-cell-title">{"REWARDS"}</h4>
-          <h4 className="cpd-cell-title">{"PAYMENT"}</h4>
+        <div className="cpd-tab-titles fl-cc">
+          <h4 className="cpd-tab-title">{"STORY"}</h4>
+          <h4 className="cpd-tab-title">{"RISKS"}</h4>
+          <h4 className="cpd-tab-title">{"REWARDS"}</h4>
+          <h4 className="cpd-tab-title">{"PAYMENT"}</h4>
         </div>
-        <div className="cpd-separator"><div className="cpd-active-cell-indic"></div></div>
+        <div className="cpd-separator"><div className="cpd-active-tab-indic"></div></div>
 
-        <div className="cpd-cell-story">
+        <div className="cpd-tab-rewards">
           
         </div>
       </main>
