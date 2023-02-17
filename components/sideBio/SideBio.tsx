537198c7154a1467f08e710aa56742fd7763694b
xapski <xapski2671@gmail.com>
2023-02-17 11:28:01 +0000

    Feb 17, 2023, 12:28 PM

--- components/sideBio/SideBio.tsx
+++ components/sideBio/SideBio.tsx
@@ -6,45 +6,44 @@ export default function SideBio() {
   return (
     <div className="sb-container fl-tl fl-c">
       <div className="sb-bio fl-tl fl-c">
-        <h4>{"Creator"}</h4>
+        <h4 className="sb-bio-heading">{"Creator"}</h4>
 
-        <div>
-          <div></div>
-          <div>
-            <div>
-              <FontAwesomeIcon icon={faEthereum}/>
+        <div className="sb-bio-details fl-tl">
+          <div className="sb-creator-jazzicon"></div>
+          <div className="sb-creator-details fl-tl fl-c">
+            <div className="sb-creator-address fl-cl">
+              <FontAwesomeIcon icon={faEthereum} className="sb-bio-curr-icon"/>
               <p>{"0x82b9...be0c"}</p>
             </div>
-            <p>{"tubbycat18"}</p>
+            <p className="sb-creator-name">{"tubbycat18"}</p>
           </div>
         </div>
 
-        <div>
+        <div className="sb-creator-socials fl-cl">
           <FontAwesomeIcon icon={faTwitter} className="acp-social-icon"/>
           <FontAwesomeIcon icon={faGlobe} className="acp-social-icon"/>
           <FontAwesomeIcon icon={faShareNodes} className="acp-social-icon"/>
         </div>
-        
-        <p>{"A VR engineer and game developer based in San Antonio"}</p>
+
+        <p className="sb-creator-bio">{"A VR engineer and game developer based in San Antonio"}</p>
       </div>
 
       <div className="sb-support fl-tl fl-c">
-        <h4>{"Support"}</h4>
-        <div>
-          <h4>{"Pledge without a reward"}</h4>
+        <h4 className="sb-support-heading">{"Support"}</h4>
+        <div className="sb-support-box fl-tl fl-c">
+          <h4 className="sb-support">{"Pledge without a reward"}</h4>
 
-          <div>
+          <div className="sb-input fl-cl">
             <p>{"ETH"}</p>
             <input type="number" />
           </div>
 
-          <div>
+          <div className="sb-support-info">
             <h4>{"Back it because you believe in it."}</h4>
             <p>{"Support the project for no reward, just because it speaks to you."}</p>
           </div>
           
           <button className="acp-fund-cta">{"Fund this project"}</button>
-
         </div>
       </div>
     </div>
--- containers/campaignPage/campaignDetails/CampaignDetails.tsx
+++ containers/campaignPage/campaignDetails/CampaignDetails.tsx
@@ -5,23 +5,24 @@ export default function CampaignDetails() {
   return (
     <section className="cpd-section fl-tc">
       <main className="cpd-trajectory">
-        <div className="cpd-tab-titles fl-cc">
-          <h4 className="cpd-tab-title">{"STORY"}</h4>
-          <h4 className="cpd-tab-title">{"RISKS"}</h4>
-          <h4 className="cpd-tab-title cpd-active-tab">{"REWARDS"}</h4>
-          <h4 className="cpd-tab-title">{"PAYMENT"}</h4>
-        </div>
-
-        <div className="cpd-separator">
-          {/* <div className="cpd-active-tab-indic"></div> */}
+        <div className="cpd-title-bar">
+          <div className="cpd-tab-titles fl-cc">
+            <div className="cpd-tab-title">{"STORY"}</div>
+            <div className="cpd-tab-title">{"RISKS"}</div>
+            <div className="cpd-tab-title cpd-active-tab">{"REWARDS"}</div>
+            <div className="cpd-tab-title">{"PAYMENT"}</div>
+          </div>
+          <div className="cpd-separator">
+            {/* <div className="cpd-active-tab-indic"></div> */}
+          </div>
         </div>
 
         <RewardsTab/>
       </main>
 
       <aside className="cpd-info">
-        <SideNotice/>
-        {/* <SideBio/> */}
+        {/* <SideNotice/> */}
+        <SideBio/>
       </aside>
     </section>
   )
