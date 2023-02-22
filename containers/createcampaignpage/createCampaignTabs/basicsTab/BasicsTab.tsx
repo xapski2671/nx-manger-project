7853dc696ffea1571f705a78c9c06a24c7b9da29
xapski <xapski2671@gmail.com>
2023-02-22 10:46:02 +0000

    Feb 22, 2023, 11:46 AM

--- containers/createcampaignpage/createCampaignTabs/basicsTab/BasicsTab.tsx
+++ containers/createcampaignpage/createCampaignTabs/basicsTab/BasicsTab.tsx
@@ -1,7 +1,7 @@
 export default function BasicsTab() {
   return (
     <div className="bt-tab" id="basics-tab">
-      <div className="bt-heading">
+      <div className="bt-heading fl-tl fl-c">
         <h2 className="bt-title">{"Basics"}</h2>
         <p className="bt-subtitle">{"Make a good first impression: introduce your campaign objectives and entice people to learn more. This basic information will represent your campaign on your campaign page, on your campaign card, and in searches."}</p>
       </div>
