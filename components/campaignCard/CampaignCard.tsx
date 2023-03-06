a0df0fa074fda6fe466ac27a7d49bb82578d7cb8
xapski <xapski2671@gmail.com>
2023-03-06 10:25:59 +0000

    Mar 6, 2023, 11:25 AM

--- components/campaignCard/CampaignCard.tsx
+++ components/campaignCard/CampaignCard.tsx
@@ -27,8 +27,11 @@ let cmpObject:cmp = {
   deadline: BigInt("0")
 }
 
+const deci = BigInt("1000000000000000000").valueOf()
+
 export default function CampaignCard({ address, creator }:props) {
   const { hasMetamask, isConnected, chainId, signer, account, connect }:conn = useContext(ConnectionContext)!
+  const [loading, setLoading] = useState(true)
   const [campaignDetails, setCampaignDetails] = useState<cmp>()
 
   useEffect(() => {
@@ -41,6 +44,7 @@ export default function CampaignCard({ address, creator }:props) {
           cmpProxy[(Object.keys(cmpObject)[i])] = cmpData[i]
         }
         setCampaignDetails(cmpProxy)
+        setLoading(false)
         console.log(cmpProxy.creator)
       }
       catch(e){console.log(e)}
@@ -50,55 +54,58 @@ export default function CampaignCard({ address, creator }:props) {
 
   return (
     <div className="cc-container fl-cl fl-c">
-      <div className="cc-img">
-        <img src="/assets/manger-mockup-cmp.jpg" alt="cc-mckp" />
-      </div>
+      { !loading &&
+ <>
+   <div className="cc-img">
+     <img src="/assets/manger-mockup-cmp.jpg" alt="cc-mckp" />
+   </div>
 
-      <div className="cc-details fl-cl fl-c">
-        <div className="cc-cta fl-tc fl-sb">
-          <div className="cc-cat-name fl-cl">
-            <FontAwesomeIcon icon={faCubes} className="cc-cat-icon"/>
-            {campaignDetails?.category}
-          </div>
-          <button className="fl-cc">{"Learn more..."}</button>
-        </div>
+   <div className="cc-details fl-cl fl-c">
+     <div className="cc-cta fl-tc fl-sb">
+       <div className="cc-cat-name fl-cl">
+         <FontAwesomeIcon icon={faCubes} className="cc-cat-icon"/>
+         {campaignDetails?.category}
+       </div>
+       <button className="fl-cc">{"Learn more..."}</button>
+     </div>
 
-        <div className="cc-camp-title fl-tl fl-c">
-          <h4>{"Dive Into The Metaverse"}</h4>
-          <p>{"Take a surreal dive into the beautiful world of web3, and have fun."}</p>
-        </div>
+     <div className="cc-camp-title fl-tl fl-c">
+       <h4>{"Dive Into The Metaverse"}</h4>
+       <p>{"Take a surreal dive into the beautiful world of web3, and have fun."}</p>
+     </div>
 
-        <div className="cc-status fl-cl fl-sb">
-          <div className="cc-amounts fl-tl fl-c">
-            <div className="cc-amt-raised fl-cl">
-              <FontAwesomeIcon icon={faEthereum} className="cc-curr-icon"/>
-              <p className="cc-amt-figure">{campaignDetails?.currentBalance.toString()}</p>
-              <p className="cc-amt-curr">{"ETH"}</p>
-            </div>
-            <div className="cc-goal">
-              {`raised out of ${campaignDetails?.goalAmount.toString()} ETH`}
-            </div>
-          </div>
+     <div className="cc-status fl-cl fl-sb">
+       <div className="cc-amounts fl-tl fl-c">
+         <div className="cc-amt-raised fl-cl">
+           <FontAwesomeIcon icon={faEthereum} className="cc-curr-icon"/>
+           <p className="cc-amt-figure">{(campaignDetails!.currentBalance.valueOf() / deci).toString()}</p>
+           <p className="cc-amt-curr">{"ETH"}</p>
+         </div>
+         <div className="cc-goal">
+           {`raised out of ${(campaignDetails!.goalAmount.valueOf() / deci).toString()} ETH`}
+         </div>
+       </div>
 
-          <div className="cc-progress-bar"><div className="cc-progress-level"></div></div>
+       <div className="cc-progress-bar"><div className="cc-progress-level"></div></div>
         
-          <div className="cc-percent fl-bl fl-c">
-            <p>{"3596%"}</p>
-            <p>{"funded"}</p>
-          </div>
-        </div>
+       <div className="cc-percent fl-bl fl-c">
+         <p>{"3596%"}</p>
+         <p>{"funded"}</p>
+       </div>
+     </div>
 
-        <div className="cc-creator-eta fl-cl fl-sb">
-          <div className="cc-creator fl-cl">
-            <div className="cc-creator-jazzicon"></div>
-            <p>{"bullishmei"}</p>
-          </div>
-          <div className="cc-eta fl-tr">
-            <p>{"13"}</p>
-            <p>{"days to go"}</p>
-          </div>
-        </div>
-      </div>
+     <div className="cc-creator-eta fl-cl fl-sb">
+       <div className="cc-creator fl-cl">
+         <div className="cc-creator-jazzicon"></div>
+         <p>{"bullishmei"}</p>
+       </div>
+       <div className="cc-eta fl-tr">
+         <p>{"13"}</p>
+         <p>{"days to go"}</p>
+       </div>
+     </div>
+   </div>
+ </>}
     </div>
   )
 }
