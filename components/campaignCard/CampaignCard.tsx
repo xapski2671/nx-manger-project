bd1558320e5d83269c3f18d8a26090829329cd3f
xapski <xapski2671@gmail.com>
2023-03-06 18:12:37 +0000

    Mar 6, 2023, 7:12 PM

--- components/campaignCard/CampaignCard.tsx
+++ components/campaignCard/CampaignCard.tsx
@@ -1,7 +1,7 @@
 import { faEthereum } from "@fortawesome/free-brands-svg-icons"
 import { faCubes } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useContext, useEffect, useState } from "react"
+import { useCallback, useContext, useEffect, useState } from "react"
 import campaignABI from "@/constants/Campaign.json"
 import { ConnectionContext } from "@/contexts/connection"
 import { cmp, conn } from "@/types"
@@ -32,27 +32,48 @@ let cmpObject:cmp = {
 export default function CampaignCard({ address, creator }:props) {
   const { hasMetamask, isConnected, chainId, signer, account, connect }:conn = useContext(ConnectionContext)!
   const [loading, setLoading] = useState(true)
-  const [campaignDetails, setCampaignDetails] = useState<cmp>()
+  const [campaignDetails, setCampaignDetails] = useState<cmp>(cmpObject)
   const [progess, setProgress] = useState(0)
   const [daysUntil, setDaysUntil] = useState(0)
   const [imageURI, setImageURI] = useState("")
   const [creatorVal, setCreatorVal] = useState("")
 
-  async function calcDetails(){
-    const plevel = (campaignDetails!.currentBalance.div(campaignDetails!.goalAmount)).toNumber() * 100
-    setProgress(plevel)
-
-    let deadline = new Date(campaignDetails!.deadline.toNumber() * 1000)
-    let dNow = new Date()
-    const days = (d1:Date, d2:Date) => {
-      let diff = d2.getTime() - d1.getTime()
-      let totalDays = Math.ceil(diff / (1000 * 3600 * 24))
-      return totalDays
+  async function startCard() {
+    const CmpCntrt = new ethers.Contract(address, campaignABI.abi, signer)
+    try{
+      const cmpData = await CmpCntrt.getCampaignDetails()
+      let cmpProxy:cmp | any = {}
+      for(let i = 0; i < cmpData.length; i++){
+        cmpProxy[(Object.keys(cmpObject)[i])] = cmpData[i]
+      }
+      setCampaignDetails(cmpProxy)
+      await calcDetails()
     }
-    const daysUntil = await days(dNow, deadline)
-    setDaysUntil(daysUntil)
+    catch(e){
+      console.log(e)
+    }
+  }
 
-    let uri = campaignDetails!.imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+  async function calcDetails(){
+    if(campaignDetails.goalAmount.toString() !== "0"){
+      const plevel = (campaignDetails.currentBalance.div(campaignDetails.goalAmount)).toNumber() * 100
+      setProgress(plevel)
+    }
+
+    if(campaignDetails.deadline.toString() !== "0"){
+      let deadline = new Date(campaignDetails.deadline.toNumber() * 1000)
+      let dNow = new Date()
+      const days = (d1:Date, d2:Date) => {
+        let diff = d2.getTime() - d1.getTime()
+        let totalDays = Math.ceil(diff / (1000 * 3600 * 24))
+        return totalDays
+      }
+      const daysUntil = await days(dNow, deadline)
+      setDaysUntil(daysUntil)
+    }
+
+    let uri = campaignDetails.imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+    console.log(uri)
     setImageURI(uri)
 
     const client = new ApolloClient({
@@ -60,93 +81,74 @@ export default function CampaignCard({ address, creator }:props) {
       cache: new InMemoryCache(),
     })
   
-    // const userData = await client
-    //   .query({
-    //     query: GET_USERNAME,
-    //     variables: { userAddress: creator }
-    //   })
-    //   .then(async (data) => {return data})
-    //   .catch(err => console.log("Error fetching data: ", err))
+    const userData = await client
+      .query({
+        query: GET_USERNAME,
+        variables: { userAddress: creator }
+      })
+      .then(async (data) => {return data})
+      .catch(err => console.log("Error fetching data: ", err))
   
-    // console.log(userData)
+    console.log(userData)
   }
 
 
 
-  useEffect(() => {
-    async function startCard(){
-      const CmpCntrt = new ethers.Contract(address, campaignABI.abi, signer)
-      try{
-        const cmpData = await CmpCntrt.getCampaignDetails()
-        let cmpProxy:cmp | any = {}
-        for(let i = 0; i < cmpData.length; i++){
-          cmpProxy[(Object.keys(cmpObject)[i])] = cmpData[i]
-        }
-        setCampaignDetails(cmpProxy)
-        setLoading(false)
-        await calcDetails()
-      }
-      catch(e){
-        console.log(e)
-      }
-    }    
+  useEffect(() => {  
     isConnected && startCard()
-  }, [isConnected, campaignDetails])
+  }, [])
 
   return (
     <div className="cc-container fl-cl fl-c">
-      { !loading &&
- <>
-   <div className="cc-img">
-     <img src={imageURI} alt="cc-mckp" />
-   </div>
+      <div className="cc-img">
+        <img src={imageURI} alt="cc-mckp" />
+      </div>
 
-   <div className="cc-details fl-cl fl-c">
-     <div className="cc-cta fl-tc fl-sb">
-       <div className="cc-cat-name fl-cl">
-         <FontAwesomeIcon icon={faCubes} className="cc-cat-icon"/>
-         {campaignDetails?.category}
-       </div>
-       <button className="fl-cc">{"Learn more..."}</button>
-     </div>
+      <div className="cc-details fl-cl fl-c">
+        <div className="cc-cta fl-tc fl-sb">
+          <div className="cc-cat-name fl-cl">
+            <FontAwesomeIcon icon={faCubes} className="cc-cat-icon"/>
+            {campaignDetails.category}
+          </div>
+          <button className="fl-cc">{"Learn more..."}</button>
+        </div>
 
-     <div className="cc-camp-title fl-tl fl-c">
-       <h4>{campaignDetails?.title}</h4>
-       <p>{campaignDetails?.description}</p>
-     </div>
+        <div className="cc-camp-title fl-tl fl-c">
+          <h4>{campaignDetails.title}</h4>
+          <p>{campaignDetails.description}</p>
+        </div>
 
-     <div className="cc-status fl-cl fl-sb">
-       <div className="cc-amounts fl-tl fl-c">
-         <div className="cc-amt-raised fl-cl">
-           <FontAwesomeIcon icon={faEthereum} className="cc-curr-icon"/>
-           <p className="cc-amt-figure">{ethers.utils.formatEther(campaignDetails!.currentBalance)}</p>
-           <p className="cc-amt-curr">{"ETH"}</p>
-         </div>
-         <div className="cc-goal">
-           {`raised out of ${ethers.utils.formatEther(campaignDetails!.goalAmount)} ETH`}
-         </div>
-       </div>
+        <div className="cc-status fl-cl fl-sb">
+          <div className="cc-amounts fl-tl fl-c">
+            <div className="cc-amt-raised fl-cl">
+              <FontAwesomeIcon icon={faEthereum} className="cc-curr-icon"/>
+              <p className="cc-amt-figure">{ethers.utils.formatEther(campaignDetails.currentBalance)}</p>
+              <p className="cc-amt-curr">{"ETH"}</p>
+            </div>
+            <div className="cc-goal">
+              {`raised out of ${ethers.utils.formatEther(campaignDetails.goalAmount)} ETH`}
+            </div>
+          </div>
 
-       <div className="cc-progress-bar"><div className="cc-progress-level" style={{ width: progess }}></div></div>
+          <div className="cc-progress-bar"><div className="cc-progress-level" style={{ width: progess }}></div></div>
         
-       <div className="cc-percent fl-bl fl-c">
-         <p>{`${progess}%`}</p>
-         <p>{"funded"}</p>
-       </div>
-     </div>
+          <div className="cc-percent fl-bl fl-c">
+            <p>{`${progess}%`}</p>
+            <p>{"funded"}</p>
+          </div>
+        </div>
 
-     <div className="cc-creator-eta fl-cl fl-sb">
-       <div className="cc-creator fl-cl">
-         <div className="cc-creator-jazzicon"></div>
-         <p>{"bullishmei"}</p>
-       </div>
-       <div className="cc-eta fl-tr">
-         <p>{daysUntil}</p>
-         <p>{"days to go"}</p>
-       </div>
-     </div>
-   </div>
- </>}
+        <div className="cc-creator-eta fl-cl fl-sb">
+          <div className="cc-creator fl-cl">
+            <div className="cc-creator-jazzicon"></div>
+            <p>{"bullishmei"}</p>
+          </div>
+          <div className="cc-eta fl-tr">
+            <p>{daysUntil}</p>
+            <p>{"days to go"}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- containers/homepage/campaigns/Campaigns.tsx
+++ containers/homepage/campaigns/Campaigns.tsx
@@ -1,16 +1,44 @@
 import { CampaignGrid, CategoryFilter } from "@/components/exportComps"
 import { GET_HOMEPAGE_CAMPAIGNS } from "@/constants/subgraphQueries"
-import { useQuery } from "@apollo/client"
+import { ConnectionContext } from "@/contexts/connection"
+import { conn } from "@/types"
+import { ApolloClient, InMemoryCache, useQuery } from "@apollo/client"
+import { useContext, useEffect, useState } from "react"
 
 export default function Campaigns() {
-  const { loading, error, data } = useQuery(GET_HOMEPAGE_CAMPAIGNS)  
+  const { isConnected }:conn = useContext(ConnectionContext)!
+  const [loading, setLoading] = useState(true)
+  const [campaigns, setCampaigns] = useState([])
+  
+  async function callCampaigns(){
+    const client = new ApolloClient({
+      uri: process.env.NEXT_PUBLIC_SUBGRAPH_URI,
+      cache: new InMemoryCache(),
+    })
+
+    const campaigns = await client
+      .query({
+        query: GET_HOMEPAGE_CAMPAIGNS
+      })
+      .then(async data => data.data.campaignAddeds)
+      .catch(error => console.log(error))
+
+    setCampaigns(campaigns)
+  }
+
+  useEffect(() => {
+    isConnected && callCampaigns()
+    setLoading(false)
+  }, [isConnected])
+  
+
 
   return (
     <section className="cp-campaigns sc-padding fl-cl fl-c">
       <CategoryFilter/>
       {
-        loading ? <p>{"Loading..."}</p> : error ? <p>{error.message}</p> : 
-          <CampaignGrid mapArray={data.campaignAddeds}/>
+        loading ? <p>{"Loading..."}</p> : 
+          <CampaignGrid mapArray={campaigns}/>
       }
     </section>
   )
