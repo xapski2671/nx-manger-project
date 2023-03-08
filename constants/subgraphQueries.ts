c208e4acd69848ce3a95b59aa990a21383d91c47
xapski <xapski2671@gmail.com>
2023-03-08 14:22:49 +0000

    Mar 8, 2023, 3:22 PM

--- constants/subgraphQueries.ts
+++ constants/subgraphQueries.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client"
 
 export const GET_HOMEPAGE_CAMPAIGNS = gql`
-  query getHomepageCampaigns(){
+  query getHomepageCampaigns{
     campaignAddeds(first: 6, orderBy: createdAt, orderDirection: desc) {
       id
       campaignAddress
